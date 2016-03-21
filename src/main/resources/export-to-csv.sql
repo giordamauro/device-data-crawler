09bf99f45f23624447d8176b5d50d5acec4e3f90
mgiorda <giordamauro@gmail.com>
2016-03-21 13:48:04 -0300

    Add csv sql script

---
+++ src/main/resources/export-to-csv.sql
@@ -0,0 +1,8 @@
+sqlite> .mode csv
+sqlite> .output {pathToFile.csv}
+
+sqlite>
+
+SELECT m.brand, m.model, m.model_alias, f.category, f.name, f.value 
+FROM device_model m INNER Join device_feature f ON m.id = f.device_id 
+ORDER BY m.brand, m.model, f.category, f.name;
